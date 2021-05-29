55cde7caa3adc6fcdd7dfd1ae241cc01919ce312
SG99036ML <sg99036ml@SG99036MLs-MacBook-Pro.local>
2021-05-29 18:22:56 +0800

    add coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,4 +20,3 @@ task("accounts", "Prints the list of accounts", async () => {
 module.exports = {
   solidity: "0.7.3",
 };
-
