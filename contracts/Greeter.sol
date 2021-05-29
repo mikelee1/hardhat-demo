2667d9e3446de54eb9901a943216739ec3dcbb19
SG99036ML <sg99036ml@SG99036MLs-MacBook-Pro.local>
2021-05-29 11:19:11 +0800

    add some command and test

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,23 +1,28 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.0;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
+// import "@Arachnid/solidity-stringutils/src/strings.sol"
 
 contract Greeter {
-  string greeting;
+    string greeting;
 
-  constructor(string memory _greeting) {
-    console.log("Deploying a Greeter with greeting:", _greeting);
-    greeting = _greeting;
-  }
+    constructor(string memory _greeting) {
+        // console.log("Deploying a Greeter with greeting:", _greeting);
+        greeting = _greeting;
+    }
 
-  function greet() public view returns (string memory) {
-    return greeting;
-  }
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
 
-  function setGreeting(string memory _greeting) public {
-    console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-    greeting = _greeting;
-  }
+    function setGreeting(string memory _greeting) public {
+        // console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
+
+    function getInvoker() public view returns (address) {
+        return msg.sender;
+    }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,14 +1,22 @@
 const { expect } = require("chai");
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
+describe("Greeter", function () {
+  let greeter;
+  beforeEach(async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    
+    greeter = await Greeter.deploy("Hello, world!");
     await greeter.deployed();
+  });
+  it("Should return the new greeting once it's changed", async function () {
     expect(await greeter.greet()).to.equal("Hello, world!");
-
     await greeter.setGreeting("Hola, mundo!");
     expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+
+  it("Should return the correct invoker", async function () {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    expect(await greeter.getInvoker()).to.equal(owner.address);
+    expect(await greeter.connect(addr1).getInvoker()).to.equal(addr1.address);
+    expect(await greeter.connect(addr2).getInvoker()).to.equal(addr2.address);
   });
 });
